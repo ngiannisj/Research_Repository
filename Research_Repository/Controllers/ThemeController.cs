45bf1a606446365b501605b4660e2ea7e211962e
ngian <ngian@DESKTOP-TDKOPQ1>
2021-06-15 13:03:55 +1000

    Add project files.

---
+++ Research_Repository/Controllers/ThemeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Research_Repository.Data;
+using Research_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Research_Repository.Controllers
+{
+    public class ThemeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ThemeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+    public IActionResult Index()
+        {
+        IEnumerable<Theme> objList = _db.Theme;
+            return View(objList);
+        }
+    }
+}
---
+++ Research_Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Research_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Research_Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Theme> Theme { get; set; }
+    }
+}
